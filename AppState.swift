47c144168c39cdfde0e41a03ae626ecdab9e2606
internessy <john@internessy.com>
2019-10-30 12:44:12 +1000

    Update AppState.swift

--- AppState.swift
+++ AppState.swift
@@ -1,4 +1,15 @@
+struct Point2D {
+  var x : Double
+  var y : Double
+}
+
 struct AppState {
   var counter : Int
   var favourites : [Int]
+}
+
+
+struct ViewState {
+  var zoom : Double
+  var origin : Point2D
 }
