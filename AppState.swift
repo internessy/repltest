9cb0c5f66b920436e64e3b1566da10ade3810899
internessy <john@internessy.com>
2019-10-31 09:41:28 +1000

    Update AppState.swift

--- AppState.swift
+++ AppState.swift
@@ -14,7 +14,22 @@ struct ViewState {
   var origin : Point2D
 }
 
-struct ArtObject
+struct ArtObject {
+  var id : UUID
+  var location : Point2D
+  var name : String
+  var children : [UUID]
+  var parent : UUID?
+}
+
+
+
 struct DrawingState {
+  var rootObject : ArtObject
   var artObjects : []
+}
+
+
+struct Color {
+  var r, g, b : Double
 }
